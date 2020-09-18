c2bedc46c2104d9f4397bd2e5fc82692c05051c9
MarielHamson <45244997+MarielHamson@users.noreply.github.com>
2020-09-18 08:49:55 -0700

    pass reducers test 2

--- src/reducers/form-visible-reducer.js
+++ src/reducers/form-visible-reducer.js
@@ -2,7 +2,7 @@ import * as c from './../actions/ActionTypes';
 
 export default (state = false, action) => {
 	switch (action.type) {
-		case c.TOGGLE_FORM:
+		case 'TOGGLE_FORM':
 			return !state;
 		default:
 			return state;
